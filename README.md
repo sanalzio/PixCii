4f3a9761d622fd9fbe292253765b7898326b01fa
Sanalzio <73523745+sanalzio@users.noreply.github.com>
2024-02-04 16:30:09 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,41 +1,61 @@
-# Image 2 Ascii
+# PixCii
 
-![NodeJS](https://img.shields.io/badge/Node.JS-%23378D3B.svg?style=for-the-badge&logo=node.js&logoColor=white) ![Node Version](https://img.shields.io/static/v1?label=Version&message=18.18.0&style=for-the-badge&labelColor=4BAE4F&color=2E7D32&logo=node.js&logoColor=ffffff) ![LICENSE](https://img.shields.io/static/v1?label=LICENSE&message=MIT&style=for-the-badge) [<img alt="downloadbtn" src="https://dabuttonfactory.com/button.png?t=Download&f=Ubuntu-Bold&ts=30&tc=fff&hp=15&vp=15&c=6&bgt=unicolored&bgc=238636&bs=4&bc=37914a" width="77px">](https://github.com/sanalzio/image2ascii/releases)
+![NodeJS](https://img.shields.io/badge/Node.JS-%23378D3B.svg?style=for-the-badge&logo=node.js&logoColor=white) ![Node Version](https://img.shields.io/static/v1?label=Version&message=18.18.0&style=for-the-badge&labelColor=4BAE4F&color=2E7D32&logo=node.js&logoColor=ffffff) ![LICENSE](https://img.shields.io/static/v1?label=LICENSE&message=GPL-V3&style=for-the-badge) [<img alt="downloadbtn" src="https://dabuttonfactory.com/button.png?t=Download&f=Ubuntu-Bold&ts=30&tc=fff&hp=15&vp=15&c=6&bgt=unicolored&bgc=238636&bs=4&bc=37914a" width="77px">](https://github.com/sanalzio/PixCii/releases)
 
-**Simple "open-source" image to ASCII text converter.**
+**Free image to ASCII text converter software.**
+[What is the free software?](https://en.wikipedia.org/wiki/Free_software)
 
 ```
 
 
-                     image2ascii X.X.X
+                     PixCii X.X.X
     Simple "open-source" image to ASCII text converter.
 
-    Usage: ./image2ascii <*input_file_path> <arguments>
-    Or you can simply drag and drop the files you want to cut into image2ascii.exe.
+    Usage: ./pixcii <*input_file_path> <arguments>
+    Or you can simply drag and drop the files you want to cut into executable file.
 
     Arguments:
 
-    --help or -h: show this menu.
+    --help or -h: show undetailed help menu.
 
-    --out or -o: resize image to specified width and height.
+    --deatiledhelp or -dh: show this menu.
+
+    --out or -o: set output file path.
     Usage: --out <output_file_path>
 
     --size or -s: resize image to specified width and height.
-    Usage: --size <width> <height>
+    Usage: --size <width (pixels)> <height (pixels)>
 
     --reverse or -r: reverse the ascii characters.
-
-    --eval or -e: evaluate the specified javascript code.
-    Usage: --eval "<javascript code>"
 
     --log or -l: print the ascii text to the console.
 
     --noinfo or -i: disable write info to console.
 
+    --savesize or -ss: disable the resizing performed to maintain the aspect ratio..
+
+    +<JIMP_Function>: run specified JIMP function before converting to ascii.
+    Example: +resize(90,24)
+
+    --exec or -e: execute the specified javascript code.
+    Usage: --exec "<javascript code>"
+
+    --onconverted or -onconed: execute the specified javascript code on converted.
+    Example: --onconed "<javascript code>"
+
+    --onsaved or -onsvd: execute the specified javascript code on saved.
+    Example: --onsvd "<javascript code>"
+
+    --onstart or -onst: execute the specified javascript code on start.
+    Example: --onst "<javascript code>"
+
+    --beforeconverting or -befcon: execute the specified javascript code before converting.
+    Example: --befcon "<javascript code>"
+
 
 ```
 
-- [Image 2 Ascii](#image-2-ascii)
+- [PixCii](#pixcii)
   - [Istallation](#istallation)
   - [Usage](#usage)
     - [Run like this:](#run-like-this)
@@ -43,27 +63,26 @@
   - [License](#license)
 
 ## Istallation
-[<img alt="downloadbtn" src="https://dabuttonfactory.com/button.png?t=Download&f=Ubuntu-Bold&ts=30&tc=fff&hp=15&vp=15&c=6&bgt=unicolored&bgc=238636&bs=4&bc=37914a" width="100px">](https://github.com/sanalzio/image2ascii/releases)
+[<img alt="downloadbtn" src="https://dabuttonfactory.com/button.png?t=Download&f=Ubuntu-Bold&ts=30&tc=fff&hp=15&vp=15&c=6&bgt=unicolored&bgc=238636&bs=4&bc=37914a" width="100px">](https://github.com/sanalzio/PixCii/releases)
 
-Download image2ascii latest relase from [here](https://github.com/sanalzio/image2ascii/releases).
+Download PixCii latest relase from [here](https://github.com/sanalzio/PixCii/releases).
 
 ## Usage
 
 ### Run like this:
 ```bash
-image2ascii <input_file_path> <arguments>
+pixcii <input_file_path> <arguments>
 ```
 
 > [!TIP]
-> **Or you can simply drag and drop the files you want to cut into image2ascii.exe**
+> **Or you can simply drag and drop the files you want to cut into executable file**
 
 ### Examples
 ```bash
-image2ascii image.png
-image2ascii image.png --size 100 50 --reverse --out output.txt
-image2ascii image.png --size 100 50 --reverse --log
-image2ascii image.png --eval "console.log('Hello World')"
+pixcii image.png
+pixcii image.png --size 100 50 --reverse --out output.txt
+pixcii image.png --size 100 50 --reverse --log
 ```
 
 ## License
-[MIT](https://mit-license.org/)
+[GPL-V3](https://www.gnu.org/licenses/gpl-3.0.en.html#license-text)
--- index.js
+++ index.js
@@ -6,31 +6,47 @@ const package = require("./package.json");
 
 async function imageToAscii(filePath) {
     try {
+        let on_converted = "";
+        let on_saved = "";
+        let before_converting = "";
+        let on_start = "";
         const image = await Jimp.read(filePath);
-        image.resize(image.bitmap.width*2, image.bitmap.height);
+        if(!(argv.includes("--savesize") || argv.includes("-ss") || argv.includes("ss"))) {
+            image.resize(image.bitmap.width*2, image.bitmap.height);
+        }
         let width = image.bitmap.width;
         let height = image.bitmap.height;
         let filename = filePath + '.txt';
         let asciiChars = '@$B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`\'. '.split('').reverse(); // '@#S%?*+;:,. '
         for (let i = 0; i < argv.length; i++) {
             const arg = argv[i];
-            if (arg === '--size' || arg === 's' || arg === '-s') {
+            if (arg.startsWith("+")) {
+                eval("image."+arg.slice(1));
+            } else if (arg === '--size' || arg === 's' || arg === '-s') {
                 image.resize(eval(argv[i+1]), eval(argv[i+2]));
-                width = image.bitmap.width;
-                height = image.bitmap.height;
-                continue;
             } else if (arg === '--reverse' || arg === 'r' || arg === '-r') {
                 asciiChars=asciiChars.reverse();
-                continue;
-            } else if (arg === '--eval' || arg === 'e' || arg === '-e') {
+            } else if (arg === '--exec' || arg === 'e' || arg === '-e') {
                 eval(argv[i+1]);
             } else if (arg === '--out' || arg === 'o' || arg === '-o') {
                 filename = argv[i+1];
+            } else if (arg === '--onconverted' || arg === '-onconed') {
+                on_converted = argv[i+1];
+            } else if (arg === '--onsaved' || arg === '-onsvd') {
+                on_saved = argv[i+1];
+            } else if (arg === '--onstart' || arg === '-onst') {
+                on_start = argv[i+1];
+            } else if (arg === '--beforeconverting' || arg === '-befcon') {
+                before_converting = argv[i+1];
             }
         }
+        eval(on_start);
+        width = image.bitmap.width;
+        height = image.bitmap.height;
 
         let asciiArt = '';
 
+        eval(before_converting);
         for (let y = 0; y < height; y++) {
             asciiArt += (argv.includes("--reverse") || argv.includes("-r") || argv.includes("r"))?Fore.Reverse:"";
             for (let x = 0; x < width; x++) {
@@ -42,31 +58,54 @@ async function imageToAscii(filePath) {
             }
             asciiArt += (argv.includes("--reverse") || argv.includes("-r") || argv.includes("r"))?Fore.Reset+'\n':"\n";
         }
+        eval(on_converted);
         if (argv.includes("--log") || argv.includes("-l") || argv.includes("l")){
             console.log((argv.includes("--reverse") || argv.includes("-r") || argv.includes("r"))?Fore.Reverse+asciiArt+Fore.Reset:asciiArt);
             process.exit(0);
         }
         writeFileSync(filename, asciiArt);
+        eval(on_saved);
         console.log(Fore.Yellow + 'Info:' + Fore.Reset, 'Ascii text was successfully written to the file named ' + Fore.Green + `"${filename}"` + Fore.Reset + '.');
     } catch (error) {
         console.error(Fore.Red + 'Error:' + Fore.Reset, error.message);
     }
 }
 if(!(argv.includes("--noinfo") || argv.includes("-i") || argv.includes("i"))) {
-    console.log(Fore.Yellow + '\n\n                     image2ascii ' + package.version + Fore.Reset);
+    console.log(Fore.Yellow + '\n\n                     PixCii ' + package.version + Fore.Reset);
     console.log('    Simple "open-source" image to ASCII text converter.\n');
 }
-if (argv.length < 3 || argv.includes("--help") || argv.includes("h") || argv.includes("-h")) {
-    console.log(Fore.Green + '    Usage' + Fore.Reset + `: ./image2ascii <${Fore.Bright}*${Fore.Reset}input_file_path> <arguments>`);
-    console.log('    Or you can simply drag and drop the files you want to cut into ' + Fore.Green + 'image2ascii.exe' + Fore.Reset + '.\n');
+if (argv.length < 3 || argv.includes("--help") || argv.includes("h") || argv.includes("-h") || argv.includes("?")) {
+    console.log(Fore.Green + '    Usage' + Fore.Reset + `: ./pixcii <${Fore.Bright}*${Fore.Reset}input_file_path> <arguments>`);
+    console.log('    Or you can simply drag and drop the files you want to cut into executable file.\n');
     console.log(Fore.Yellow + '    Arguments' + Fore.Reset + `:\n`);
     console.log(Fore.BrightBlue + '    --help' + Fore.Reset + ' or ' + Fore.BrightBlue + '-h' + Fore.Reset + `: show this menu.\n`);
-    console.log(Fore.BrightBlue + '    --out' + Fore.Reset + ' or ' + Fore.BrightBlue + '-o' + Fore.Reset + `: resize image to specified width and height.\n` + Fore.Green + '    Usage' + Fore.Reset + `: --out <output_file_path>\n`);
-    console.log(Fore.BrightBlue + '    --size' + Fore.Reset + ' or ' + Fore.BrightBlue + '-s' + Fore.Reset + `: resize image to specified width and height.\n` + Fore.Green + '    Usage' + Fore.Reset + `: --size <width> <height>\n`);
+    console.log(Fore.BrightBlue + '    --deatiledhelp' + Fore.Reset + ' or ' + Fore.BrightBlue + '-dh' + Fore.Reset + `: show detailed help menu.\n`);
+    console.log(Fore.BrightBlue + '    --out' + Fore.Reset + ' or ' + Fore.BrightBlue + '-o' + Fore.Reset + `: set output file path.\n` + Fore.Green + '    Usage' + Fore.Reset + `: --out <output_file_path>\n`);
+    console.log(Fore.BrightBlue + '    --size' + Fore.Reset + ' or ' + Fore.BrightBlue + '-s' + Fore.Reset + `: resize image to specified width and height.\n` + Fore.Green + '    Usage' + Fore.Reset + `: --size <width (pixels)> <height (pixels)>\n`);
     console.log(Fore.BrightBlue + '    --reverse' + Fore.Reset + ' or ' + Fore.BrightBlue + '-r' + Fore.Reset + `: reverse the ascii characters.\n`);
-    console.log(Fore.BrightBlue + '    --eval' + Fore.Reset + ' or ' + Fore.BrightBlue + '-e' + Fore.Reset + `: evaluate the specified javascript code.\n` + Fore.Green + '    Usage' + Fore.Reset + `: --eval "<javascript code>"\n`);
     console.log(Fore.BrightBlue + '    --log' + Fore.Reset + ' or ' + Fore.BrightBlue + '-l' + Fore.Reset + `: print the ascii text to the console.\n`);
     console.log(Fore.BrightBlue + '    --noinfo' + Fore.Reset + ' or ' + Fore.BrightBlue + '-i' + Fore.Reset + `: disable write info to console.\n`);
+    console.log(Fore.BrightBlue + '    --savesize' + Fore.Reset + ' or ' + Fore.BrightBlue + '-ss' + Fore.Reset + `: disable the resizing performed to maintain the aspect ratio..\n`);
+    console.log("");
+    process.exit(0);
+} else if (argv.length < 3 || argv.includes("--deatiledhelp") || argv.includes("dh") || argv.includes("-dh") || argv.includes("?+")) {
+    console.log(Fore.Green + '    Usage' + Fore.Reset + `: ./pixcii <${Fore.Bright}*${Fore.Reset}input_file_path> <arguments>`);
+    console.log('    Or you can simply drag and drop the files you want to cut into executable file.\n');
+    console.log(Fore.Yellow + '    Arguments' + Fore.Reset + `:\n`);
+    console.log(Fore.BrightBlue + '    --help' + Fore.Reset + ' or ' + Fore.BrightBlue + '-h' + Fore.Reset + `: show undetailed help menu.\n`);
+    console.log(Fore.BrightBlue + '    --deatiledhelp' + Fore.Reset + ' or ' + Fore.BrightBlue + '-dh' + Fore.Reset + `: show this menu.\n`);
+    console.log(Fore.BrightBlue + '    --out' + Fore.Reset + ' or ' + Fore.BrightBlue + '-o' + Fore.Reset + `: set output file path.\n` + Fore.Green + '    Usage' + Fore.Reset + `: --out <output_file_path>\n`);
+    console.log(Fore.BrightBlue + '    --size' + Fore.Reset + ' or ' + Fore.BrightBlue + '-s' + Fore.Reset + `: resize image to specified width and height.\n` + Fore.Green + '    Usage' + Fore.Reset + `: --size <width (pixels)> <height (pixels)>\n`);
+    console.log(Fore.BrightBlue + '    --reverse' + Fore.Reset + ' or ' + Fore.BrightBlue + '-r' + Fore.Reset + `: reverse the ascii characters.\n`);
+    console.log(Fore.BrightBlue + '    --log' + Fore.Reset + ' or ' + Fore.BrightBlue + '-l' + Fore.Reset + `: print the ascii text to the console.\n`);
+    console.log(Fore.BrightBlue + '    --noinfo' + Fore.Reset + ' or ' + Fore.BrightBlue + '-i' + Fore.Reset + `: disable write info to console.\n`);
+    console.log(Fore.BrightBlue + '    --savesize' + Fore.Reset + ' or ' + Fore.BrightBlue + '-ss' + Fore.Reset + `: disable the resizing performed to maintain the aspect ratio..\n`);
+    console.log(Fore.BrightBlue + '    +<JIMP_Function>' + Fore.Reset + `: run specified JIMP function before converting to ascii.\n` + Fore.Green + '    Example' + Fore.Reset + `: +resize(90,24)\n`);
+    console.log(Fore.BrightBlue + '    --exec' + Fore.Reset + ' or ' + Fore.BrightBlue + '-e' + Fore.Reset + `: execute the specified javascript code.\n` + Fore.Green + '    Usage' + Fore.Reset + `: --exec "<javascript code>"\n`);
+    console.log(Fore.BrightBlue + '    --onconverted' + Fore.Reset + ' or ' + Fore.BrightBlue + '-onconed' + Fore.Reset + `: execute the specified javascript code on converted.\n` + Fore.Green + '    Example' + Fore.Reset + `: --onconed "<javascript code>"\n`);
+    console.log(Fore.BrightBlue + '    --onsaved' + Fore.Reset + ' or ' + Fore.BrightBlue + '-onsvd' + Fore.Reset + `: execute the specified javascript code on saved.\n` + Fore.Green + '    Example' + Fore.Reset + `: --onsvd "<javascript code>"\n`);
+    console.log(Fore.BrightBlue + '    --onstart' + Fore.Reset + ' or ' + Fore.BrightBlue + '-onst' + Fore.Reset + `: execute the specified javascript code on start.\n` + Fore.Green + '    Example' + Fore.Reset + `: --onst "<javascript code>"\n`);
+    console.log(Fore.BrightBlue + '    --beforeconverting' + Fore.Reset + ' or ' + Fore.BrightBlue + '-befcon' + Fore.Reset + `: execute the specified javascript code before converting.\n` + Fore.Green + '    Example' + Fore.Reset + `: --befcon "<javascript code>"\n`);
     console.log("");
     process.exit(0);
 }
